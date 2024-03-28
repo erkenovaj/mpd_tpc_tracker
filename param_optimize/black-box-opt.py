d8c3a609ce3c1a00410d5c0f60d19327f8bc26d5
Veronika Burdelnaya <nika.burdelnaya@gmail.com>
2024-03-28 20:16:51 +0300

    Update params, fix logfile name

--- param_optimize/black-box-opt.py
+++ param_optimize/black-box-opt.py
@@ -1,28 +1,25 @@
+import os
 import json
 import logging
 import warnings
+import datetime
 import optuna
 from acts_launcher import run
 
-logging.basicConfig(filename='param_history.json',
-                    filemode='w', encoding='utf-8',
-                    level=logging.INFO,
-                    format='%(message)s')
 warnings.filterwarnings("ignore", category=optuna.exceptions.ExperimentalWarning)
 
-NUM_TRIALS = 100
+NUM_TRIALS = 200
+LOG_DIR = 'log_params'
+
 search_space = {}
 
 fixed_params = {
-    "PtMin": 0.0,
-    "CollisionZmin": -30.0,
-    "CollisionZmax": 30.0,
-    "BeamX": 0.0,
-    "BeamY": 0.0,
+    "CollisionZmin": -300.0,
+    "CollisionZmax": 300.0,
     "SigmaLoc0": 0.5,
     "SigmaLoc1": 0.5,
-    "SigmaPhi": 0.5,
-    "SigmaTheta": 0.5,
+    "SigmaPhi": 0.00872665,
+    "SigmaTheta": 0.00872665,
     "SigmaQOverP": 0.1,
     "SigmaT0": 2000.0,
     "InitVarInflatLoc0": 1.0,
@@ -41,13 +38,12 @@ fixed_params = {
     "TrackMinLength": 4,
     "NewHitsInRow": 6,
     "NewHitsRatio": 0.25,
-    "PostProcess": True,
+    "PostProcess": False,
     "SelectorEnabled": True,
     "PrimaryParticlesOnly": True,
     "SelectorPhiMin": -3.15,
     "SelectorPhiMax": 3.15,
     "AbsEtaMin": 0,
-    "SelectorPtMax": 10.0,
     "KeepNeutral": False,
     "NHitsMin": 9,
     "TruthMatchProbMin": 0.5,
@@ -111,17 +107,27 @@ def objective(trial):
     SeedBinSizeR = trial.suggest_int("SeedBinSizeR", 6, 36, step=1)
     SeedDeltaRmin = trial.suggest_int("SeedDeltaRmin", 5, 20, step=1)
     SeedDeltaRmax = trial.suggest_int("SeedDeltaRmax", 30, 120, step=2)
-    SeedDeltaZmax = trial.suggest_int("SeedDeltaZmax", 10, 40, step=2)
+    SeedDeltaZmax = trial.suggest_int("SeedDeltaZmax", 100, 400, step=2)
     SigmaScattering = trial.suggest_int("SigmaScattering", 1, 10, step=1)
     MaxPtScattering = trial.suggest_int("MaxPtScattering", 1, 10, step=1)
     RadLengthPerSeed = trial.suggest_float("RadLengthPerSeed", 0.01, 0.1, step=0.01)
     Chi2max = trial.suggest_int("Chi2max", 15, 60, step=5)
     MaxSeedsPerSpM = trial.suggest_int("MaxSeedsPerSpM", 1, 10, step=1)
     ImpactMax = trial.suggest_int("ImpactMax", 10, 100, step=5)
-    PropagationMaxSteps = trial.suggest_int("PropagationMaxSteps", 100, 10000, step=100)
+    PropagationMaxSteps = trial.suggest_int("PropagationMaxSteps", 100, 1000, step=100)
     NmaxPerSurface = trial.suggest_int("NmaxPerSurface", 1, 10, step=1)
+    SelectorPtMax = trial.suggest_int("SelectorPtMax", 2, 10, step=1)
+    EtaMax = trial.suggest_float("EtaMax", 1.8, 2.2, step=0.04)
+    PtMin = trial.suggest_float("PtMin", 0.04, 0.1, step=0.002)
+    BeamX = trial.suggest_int("BeamX", 0, 10, step=1)
+    BeamY = trial.suggest_int("BeamY", 0, 10, step=1)
 
     dct_params = {
+        "SelectorPtMax": SelectorPtMax,
+        "EtaMax": EtaMax,
+        "PtMin": PtMin,
+        "BeamX": BeamX,
+        "BeamY": BeamY,
         "SeedBinSizeR": SeedBinSizeR,
         "SeedDeltaRmin": SeedDeltaRmin,
         "SeedDeltaRmax": SeedDeltaRmax,
@@ -142,11 +148,23 @@ def objective(trial):
 def main():
     '''main func'''
 
+    if not os.path.isdir(LOG_DIR):
+        new_dirpath = os.path.join(os.getcwd(), LOG_DIR)
+        os.mkdir(new_dirpath)
+
+    curr_time = datetime.datetime.now()
+    timestamp = datetime.datetime.strftime(curr_time, "%Y-%d-%m-%H-%M-%S")
+    logging.basicConfig(filename=f'{LOG_DIR}/param_history_{timestamp}.json',
+                        filemode='w', encoding='utf-8',
+                        level=logging.INFO,
+                        format='%(message)s'
+                        )
+
     study = optuna.create_study(
         direction='maximize',
-        sampler=samplers_optuna['tpe']
+        sampler=samplers_optuna['random']
         )
-    study.optimize(objective, n_trials=NUM_TRIALS, callbacks=[write_log])
+    study.optimize(objective, n_trials=NUM_TRIALS, callbacks=[write_log], n_jobs=1)
     best_params = {repr(key): val for key, val in study.best_params.items()}
 
     logging.info(f'"best":\n\t{{"eff": {study.best_value}, \n\t"params": {best_params}}}')
